07504655cbc0c8c797726febafdf156b256ac05c
Britney Parkerson <justbritneyp1989@gmail.com>
2020-03-14 01:44:42 -0400

    still working on it

---
+++ user-card/src/components/searchform.js
@@ -0,0 +1,10 @@
+ import React from "react";
+
+ function Search() {
+     return (
+         <form>
+             <button>Find Followers</button> <input type="text" placeholder="username"/>
+         </form>
+     )
+ }
+ export default Search;
